8f41d79e7aded65fd7470bf5591c930dcf983038
yuzhang <yuzhang2002@gmail.com>
2022-02-19 14:52:18 +1100

    [DONE] added array.slice exercises

---
+++ DataStructure/array/sliceIndexPractice.js
@@ -0,0 +1,7 @@
+const a = [1,2,3,4,5,6,7];
+
+console.log(`${a.slice(0)}`);
+console.log(`${a.slice(0,)}`);
+console.log(`${a.slice(0,1)}`);
+console.log(`${a.slice(0,2)}`);
+console.log(`${a.slice(2,5)}`);

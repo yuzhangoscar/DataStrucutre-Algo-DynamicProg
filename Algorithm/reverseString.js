220c50df75a070bb23487aa36b63958f96d7607a
yuzhang <yuzhang2002@gmail.com>
2022-03-13 16:03:03 +1100

    [DONE] added solution for reverString

--- Algorithm/reverseString.js
+++ Algorithm/reverseString.js
@@ -1,14 +1,27 @@
 // Write a function that reverses a string. The input string is given as an array of characters s.
-
 // You must do this by modifying the input array in-place with O(1) extra memory.
 
- 
 
 // Example 1:
-
 // Input: s = ["h","e","l","l","o"]
 // Output: ["o","l","l","e","h"]
-// Example 2:
 
+// Example 2:
 // Input: s = ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
+
+const reverseString = (s) => {
+    const stringLength = s.length;
+    let startIndex = 0;
+    let endIndex = stringLength - 1;
+
+    while(endIndex > startIndex) {
+        [s[startIndex], s[endIndex]] = [s[endIndex], s[startIndex]];
+        endIndex--;
+        startIndex++;
+    }
+    return s;
+};
+
+console.log(reverseString(["h","e","l","l","o"]));
+console.log(reverseString(["H","a","n","n","a","h"]));

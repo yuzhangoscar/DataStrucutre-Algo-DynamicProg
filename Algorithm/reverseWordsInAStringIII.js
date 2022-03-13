f865a24c07289ed7a64e5add130248c7273cfadc
yuzhang <yuzhang2002@gmail.com>
2022-03-13 16:36:39 +1100

    [DONE] added solution for reverse words in a string III

--- Algorithm/reverseWordsInAStringIII.js
+++ Algorithm/reverseWordsInAStringIII.js
@@ -1,6 +1,5 @@
-// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
-
- 
+// Given a string s, reverse the order of characters in each word within a 
+// sentence while still preserving whitespace and initial word order.
 
 // Example 1:
 
@@ -10,3 +9,27 @@
 
 // Input: s = "God Ding"
 // Output: "doG gniD"
+
+const reverseWords = (s) => {
+    const splitWordArray = s.split(' ');
+    const newArray = [];
+
+    splitWordArray.map((eachWord) => {
+        const charArray = eachWord.split('');
+        const length = charArray.length;
+        let startIndex = 0;
+        let endIndex = length - 1;
+
+        while(endIndex > startIndex) {
+            [charArray[startIndex], charArray[endIndex]] = [charArray[endIndex], charArray[startIndex]];
+            startIndex++;
+            endIndex--;
+        }
+
+        newArray.push(charArray.join(''));
+    });
+
+    return newArray.join(' ');
+};
+
+console.log(reverseWords("Let's take LeetCode contest"));

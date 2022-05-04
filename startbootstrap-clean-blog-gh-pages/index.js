ac16f6c1cc889059502f20b9bd15dd18dfd3dcc0
yuzhang <yuzhang2002@gmail.com>
2022-05-04 17:14:30 +1000

    [PAGE]

--- startbootstrap-clean-blog-gh-pages/index.js
+++ startbootstrap-clean-blog-gh-pages/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
+const path = require('path');
 const app = new express();
 
 app.use(express.static('public'));
-
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+app.get('/about', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+app.get('/contact', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+app.get('/post', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
 app.listen(3000, ()=>{
     console.log('listening on 3000');
 });

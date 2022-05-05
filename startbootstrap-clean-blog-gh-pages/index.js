3afc56b6dc8a3c9e0b91b3e63af93a020b8b0bc0
yuzhang <yuzhang2002@gmail.com>
2022-05-05 14:31:32 +1000

    working again

--- startbootstrap-clean-blog-gh-pages/index.js
+++ startbootstrap-clean-blog-gh-pages/index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const path = require('path');
-const app = new express();
+const ejs = require('ejs');
 
-app.use(express.static('public'));
+const app = new express();
+app.set('view engine', 'ejs');
+
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
 });
 app.get('/about', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 });
 app.get('/contact', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 });
 app.get('/post', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post');
 });
+app.use(express.static('public'));
 app.listen(3000, ()=>{
     console.log('listening on 3000');
 });

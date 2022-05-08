10b3d8dd36fa3c0afa1afec380a2f7ac65ae3189
yuzhang <yuzhang2002@gmail.com>
2022-05-09 08:43:38 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/index.js
+++ startbootstrap-clean-blog-gh-pages/index.js
@@ -1,40 +1,25 @@
 const express = require('express');
-const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
-const BlogPost = require('./models/BlogPost');
+const fileUpload = require('express-fileupload');
+const newPostController = require('./controllers/newPost');
+const homeController = require('./controllers/home');
+const getPostController = require('./controllers/getPost');
+const storePostController = require('./controllers/storePost');
+const newUserController = require('./controllers/newUser');
 
 const app = new express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', async (req,res) => {
-    const blogposts = await BlogPost.find();
-    res.render('index', {
-        blogposts
-    });
-});
-app.get('/about', (req,res) => {
-    res.render('about');
-});
-app.get('/contact', (req,res) => {
-    res.render('contact');
-});
-app.get('/post/:id', async(req,res) => {
-    const blogpost = await BlogPost.findById(req.params.id);
-    res.render('post',{
-        blogpost
-    });
-});
-app.get('/posts/new', (req, res) => {
-    res.render('create');
-});
-app.post('/posts/store', async (req, res) => {
-    await BlogPost.create(req.body);
-    res.redirect('/');
-});
-app.use(express.static('public'));
+app.get('/', homeController);
+app.get('/post/:id', getPostController);
+app.get('/posts/new', newPostController);
+app.get('/users/register', newUserController);
+app.post('/posts/store', storePostController);
 app.listen(3000, ()=>{
     console.log('listening on 3000');
 });

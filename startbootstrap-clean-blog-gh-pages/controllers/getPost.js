353d81c542a5bfb611151fa96d975f1846788788
yuzhang <yuzhang2002@gmail.com>
2022-05-13 14:57:47 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/controllers/getPost.js
+++
@@ -1,8 +0,0 @@
-const BlogPost = require('../models/BlogPost');
-
-module.exports = async(req,res) => {
-    const blogpost = await BlogPost.findById(req.params.id).populate('userid');
-    res.render('post',{
-        blogpost
-        });
-};
--- startbootstrap-clean-blog-gh-pages/middleware/redirectIfAuthenticatedMiddleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if(req.session.userId) {
-        console.log('existing user detected');
-        res.redirect('/');
-    }
-    else next();
-}

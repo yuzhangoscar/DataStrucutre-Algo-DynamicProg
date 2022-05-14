d7ad20dc6e1c42244b3096b1c3722931caebbe61
yuzhang <yuzhang2002@gmail.com>
2022-05-14 16:38:59 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/index.js
+++ startbootstrap-clean-blog-gh-pages/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
+const BlogPost = require('./models/BlogPost');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+BlogPost.create({
+    title:'my title',
+    body: 'my body'
+},(error, blogpost)=>{
+    console.log(`created`);
+});
 const app = new express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -14,6 +23,9 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 app.get('/post', (req, res) => {
+    res.render('post');
+});
+app.get('/posts/new', (req, res) => {
     res.render('post');
 });
 

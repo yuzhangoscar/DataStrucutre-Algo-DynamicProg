1ee07d6f5e0d666ca89e5cc5a35a22ea171bb55e
yuzhang <yuzhang2002@gmail.com>
2022-05-15 14:11:08 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/index.js
+++ startbootstrap-clean-blog-gh-pages/index.js
@@ -3,6 +3,7 @@ const ejs = require('ejs');
 const BlogPost = require('./models/BlogPost');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const newPostController = require('./controllers/newPost');
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 const app = new express();
@@ -10,24 +11,14 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/', async (req, res) => {
     const blogposts = await BlogPost.find({});
     res.render('index', {
         blogposts
     });
 });
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-app.get('/contact', (req, res) => {
-    res.render('contact');
-});
-app.get('/post', (req, res) => {
-    res.render('post');
-});
-app.get('/posts/new', (req, res) => {
-    res.render('create');
-});
+app.get('/posts/new', newPostController);
 app.get('/post/:id', async (req, res) => {
     const blogpost = await BlogPost.findById(req.params.id);
     res.render('post', {

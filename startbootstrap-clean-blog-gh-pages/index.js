e3229063e23628a78b35b6d2c9c290a488c92404
yuzhang <yuzhang2002@gmail.com>
2022-05-15 12:35:57 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/index.js
+++ startbootstrap-clean-blog-gh-pages/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const BlogPost = require('./models/BlogPost');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 BlogPost.create({
@@ -13,7 +14,10 @@ BlogPost.create({
 const app = new express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({});
     res.render('index');
 });
 app.get('/about', (req, res) => {
@@ -29,7 +33,9 @@ app.get('/posts/new', (req, res) => {
     res.render('create');
 });
 app.post('/posts/store', (req, res) => {
-    //{title, description} = req.body;
-})
+    BlogPost.create(req.body, (error, blogpost) => {
+        res.redirect('/');
+    });
+});
 
 app.listen(3000);

d4c10a3431dc206ebf01921d8e4c36c08a299598
yuzhang <yuzhang2002@gmail.com>
2022-05-15 13:55:13 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/models/BlogPost.js
+++ startbootstrap-clean-blog-gh-pages/models/BlogPost.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    datePosted: {
+        type: Date,
+        default: new Date()
+    }
 });
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 

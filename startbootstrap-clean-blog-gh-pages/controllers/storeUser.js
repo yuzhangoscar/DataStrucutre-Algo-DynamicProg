d9314630589d236da83ad72dc9d230125ea96eb5
yuzhang <yuzhang2002@gmail.com>
2022-05-16 21:44:42 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/controllers/storeUser.js
+++ startbootstrap-clean-blog-gh-pages/controllers/storeUser.js
@@ -1,7 +1,11 @@
+const { response } = require('express');
 const User = require('../models/User');
 
 module.exports = (req, res) => {
     User.create(req.body, (error, user) => {
+        if(error) {
+            return res.redirect('/users/register');
+        }
         res.redirect('/');
     });
 };

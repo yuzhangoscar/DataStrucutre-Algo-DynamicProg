4a3adab66974475615a2031a445bc1ff47cd6f28
yuzhang <yuzhang2002@gmail.com>
2022-05-16 21:36:25 +1000

    wip

--- startbootstrap-clean-blog-gh-pages/models/User.js
+++ startbootstrap-clean-blog-gh-pages/models/User.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
-const BlogPostSchema = new Schema({
+const UserSchema = new Schema({
     username: String,
     password: String,
 });
-const User = mongoose.model('User', BlogPostSchema);
+UserSchema.pre('save', function(next) {
+    const user = this;
 
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash;
+        next();
+    });
+});
+
+const User = mongoose.model('User', UserSchema);
 module.exports = User;

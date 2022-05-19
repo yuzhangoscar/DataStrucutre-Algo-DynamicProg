758cf377bc460844b444c09bde1306af351a9e3d
yuzhang <yuzhang2002@gmail.com>
2022-05-20 07:26:11 +1000

    wip

---
+++ MERN/public/App.js
@@ -0,0 +1,4 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello world"));
+ReactDOM.render(element, document.getElementById('content'));

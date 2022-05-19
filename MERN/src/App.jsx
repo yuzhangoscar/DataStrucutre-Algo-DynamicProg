1cdd074a49d563c8235ff848c47199f703e2a949
yuzhang <yuzhang2002@gmail.com>
2022-05-20 08:59:48 +1000

    wip

--- MERN/src/App.jsx
+++ MERN/src/App.jsx
@@ -1,6 +1,51 @@
-const element = (
-    <div title="Outer div">
-        <h1>Hello world</h1>
-    </div>
-);
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>
+                Placeholder for IssueFilter
+            </div>
+        )
+    }
+}
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow />
+                    <IssueRow />
+                </tbody>
+            </table>
+        )
+    }
+}
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>
+                Placeholder for IssueAdd
+            </div>
+        )
+    }
+}
+class IssueList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <IssueTable />
+                <IssueAdd />
+            </div>
+        )
+    }
+}
+
+const element = <IssueList />;
 ReactDOM.render(element, document.getElementById('content'));

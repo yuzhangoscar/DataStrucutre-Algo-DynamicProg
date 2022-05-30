5790fbb98dd4e2da3807f9b081982a136f5032c7
yuzhang <yuzhang2002@gmail.com>
2022-05-30 21:41:47 +1000

    wip

--- MERN/public/App.js
+++ MERN/public/App.js
@@ -12,5 +12,19 @@ var IssueAdd = function IssueAdd() {
   return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for the issue add");
 };
 
-var Element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueAdd, null));
+var IssueRow = function IssueRow(props) {
+  var style = {
+    border: "1px solid silver",
+    padding: 4
+  };
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("td", {
+    style: style
+  }, "Issue row id = ", props.id));
+};
+
+var Element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, null), /*#__PURE__*/React.createElement(IssueRow, {
+  id: 1
+}), /*#__PURE__*/React.createElement(IssueRow, {
+  id: 2
+}), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueAdd, null));
 ReactDOM.render(Element, document.getElementById('content'));
--- MERN/src/App.jsx
+++ MERN/src/App.jsx
@@ -22,12 +22,23 @@ const IssueAdd = () =>{
     )
 };
 
+const IssueRow = (props) => {
+    const style = {border: "1px solid silver", padding: 4};
+    return (
+        <div>
+            <td style={style}>Issue row id = {props.id}</td>
+        </div>
+    )
+}
+
 const Element = 
     <div>
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
         <IssueTable />
+        <IssueRow id={1} />
+        <IssueRow id={2} />
         <hr />
         <IssueAdd />
     </div>;

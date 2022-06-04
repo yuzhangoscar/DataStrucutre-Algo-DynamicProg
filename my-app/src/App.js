0ce5b1e13066d8fce284e36e4f4041966b188eab
yuzhang <yuzhang2002@gmail.com>
2022-06-04 18:02:24 +1000

    wip

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,30 @@
 import Header from './Header';
+import Item from './Item';
 import './App.css';
+
+const Products = [
+    {
+      icon: '🍦',
+      price: 3
+    },
+    {
+      icon: '🍩',
+      price: 4
+    },
+    {
+      icon: '🍉',
+      price: 5
+    }
+
+];
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div>
+            {Products.map(product => (<Item icon={product.icon} price={product.price}/>))}
+      </div>
     </div>
   );
 }

6560a9612bd43b7417d78cc77c649a42aff86fba
yuzhang <yuzhang2002@gmail.com>
2022-06-09 20:36:16 +1000

    wip

--- my-php/index.php
+++ my-php/index.php
@@ -3,6 +3,8 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?> 
+        <?php echo $_SERVER['DOCUMENT_ROOT'] ?> 
+        <?php echo $_SERVER['SERVER_NAME'] ?> 
+        <?php echo "hello" ?>
     </body>
 </html>

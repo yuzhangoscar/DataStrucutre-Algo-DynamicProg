0b4f60259a54fb47aa1cedcdaba348361697c1f0
yuzhang <yuzhang2002@gmail.com>
2022-06-12 09:21:11 +1000

    wip

--- my-php/index.php
+++ my-php/index.php
@@ -4,10 +4,21 @@
     </head>
     <body>
         <?php
+            function longdate() {
+                return date("l F jS Y");
+            }
             $username = "fred";
             echo $username;
             echo "<br>";
-            echo $username;
+            $aaa = __NAMESPACE__;
+            echo __LINE__;
+            echo __FUNCTION__;
+            echo "<br>";
+            echo __DIR__;
+            echo "<br>";
+            echo "<h1>THIS is the line " . __NAMESPACE__ . "</h1>";
+            echo "<br>";
+            echo longdate();
         ?>
     </body>
 </html>

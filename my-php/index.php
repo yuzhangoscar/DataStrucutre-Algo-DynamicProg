bb6ec643e69bd85371a6382742c204361a534b67
yuzhang <yuzhang2002@gmail.com>
2022-06-13 11:44:23 +1000

    wip

--- my-php/index.php
+++ my-php/index.php
@@ -1,12 +1,17 @@
 <?php 
     namespace mySpace;
+
+    if (isset($_POST['name'])) $name=$_POST['name'];
+    else $name = "(NOT PROVIDED)";
+
     echo <<<_END
         <html>
             <head>
                 <title>PHP Test</title>
             </head>
             <body>
-                <form method="post" action="formtest.php">
+                Your name is: $name<br>
+                <form method="post" action="index.php">
                     what is yoru name?
                     <input type="text" name="name">
                     <input type="submit">

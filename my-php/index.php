82dc4961192c490b3058f3c63d676aa98918a416
yuzhang <yuzhang2002@gmail.com>
2022-06-21 21:54:20 +1000

    wip

--- my-php/index.php
+++ my-php/index.php
@@ -16,18 +16,7 @@
                     <input type="text" name="name" value="yz">
                     <input type="submit">
                 </form>
-                <textarea name="text" cols="100" rows="5" wrap="type">
-                    this is default text
-                </textarea>
-                8am-Noon<input type="radio" name="time" value="1">
-                8am-Noon<input type="radio" name="time" value="2" checked="checked">
-                8am-Noon<input type="radio" name="time" value="3">
-                A<input type="radio" name="location" value="1">
-                B<input type="radio" name="location" value="2">
-                <br>
-                <select name="name" size="1">
-                    <option value="Peas">peas</option>
-                    <option value="beans">beans</option>
+                <input type="submit" value="Search">
             </body>
         </html>
 _END;

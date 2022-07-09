ea3c10e42eb1f1ee846e32aa0d371fac27fc50b5
yuzhangoscar <yuzhang2002@gmail.com>
2022-07-09 16:07:38 +1000

    wip

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ button.addEventListener('click', () => {
 
     document.body.style.backgroundColor='rgb('+ R + ', '+ G + ', '+ B + ')';
     text.textContent = `RGB(${R}, ${G}, ${B})`;
+    console.log('clicked');
     console.log(`new color code should be ${R}, ${G}, ${B}`);
 });
 

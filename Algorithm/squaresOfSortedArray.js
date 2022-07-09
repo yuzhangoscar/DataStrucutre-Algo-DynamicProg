27671f919851d53b89b8c1ac0a4ce815c75ce863
yuzhangoscar <yuzhang2002@gmail.com>
2022-07-09 15:54:10 +1000

    deleted

--- Algorithm/squaresOfSortedArray.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var sortedSquares = function(nums) {
-    const squaredArray = nums.map(x => x*x);
-    return squaredArray.sort((a,b) => a-b);
-};
--- MERN/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const fileServerMiddleware = express.static('public');
-
-const app = express();
-app.use('/', fileServerMiddleware);
-app.listen(3000, function() {
-    console.log('lisstening on 3000');
-});

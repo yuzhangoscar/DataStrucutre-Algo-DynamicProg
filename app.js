fb29086ce1f3169d4a7a5ceb010a44f77b66ee78
yuzhang <yuzhang2002@gmail.com>
2022-07-24 21:50:33 +1000

    wip

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-let button = document.getElementById('btn');
-let text = document.getElementsByClassName('color');
+const elements = document.getElementsByTagName('h1');
+const button = document.getElementById('btn');
 
-console.log(text);
+function buttonEventHandler() {
+    let index = elements.length;
+    for (let i = index - 1; i >= 0; i--) {
+        elements[i].innerHTML = 'hhh';
+    }
+}
 
-button.addEventListener('click', () => {
-    let R = generateRandomNumber(255);
-    let G = generateRandomNumber(255);
-    let B = generateRandomNumber(255);
-    
-    console.log('clicked');
-    document.body.style.backgroundColor='rgb('+ R + ', '+ G + ', '+ B + ')';
-    text.textContent = `RGB(${R}, ${G}, ${B})`;
-    console.log(`new color code should be ${R}, ${G}, ${B}`);
-});
-
-function generateRandomNumber(max) {
-    let rand = Math.random() * max;
-
-    rand=Math.floor(rand);
-
-    return rand;
-};
+button.addEventListener('click', buttonEventHandler);

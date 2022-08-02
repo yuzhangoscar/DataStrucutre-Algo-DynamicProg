44396054191a15aa73a61ddbe29e9cd5f940d62d
yuzhang <yuzhang2002@gmail.com>
2022-08-03 08:05:05 +1000

    wwip

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const simplePlan = `
-.................
-..#...........#..
-..#.........=.#..
-`;
+// const simplePlan = `
+// .................
+// ..#...........#..
+// ..#.........=.#..
+// `;
 
-let rows = simplePlan.trim().split('\n').map(l=>[...l]);
+// let rows = simplePlan.trim().split('\n').map(l=>[...l]);
 
-console.log(rows);
+// console.log(rows);
 
-console.log(Object.prototype.toString.call(rows));
+// console.log(Object.prototype.toString.call(rows));
+
+const trivialArray = [1,2,3];
+console.log(trivialArray.length);
+trivialArray.salary = 9;
+console.log(trivialArray.length);

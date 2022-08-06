311cc998219d0787c2346af668610bfaee1f9d66
yuzhang <yuzhang2002@gmail.com>
2022-08-06 22:13:49 +1000

    wip

--- app.js
+++ app.js
@@ -10,7 +10,26 @@
 
 // console.log(Object.prototype.toString.call(rows));
 
-const trivialArray = [1,2,3];
-console.log(trivialArray.length);
-trivialArray.salary = 9;
-console.log(trivialArray.length);
+// const trivialArray = [1,2,3];
+// console.log(trivialArray.length);
+// trivialArray['salary'] = 9;
+// console.log(trivialArray['salary']);
+// console.log(trivialArray.length);
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(9.88), 1000);
+});
+
+myPromise.then(result => console.log(result));
+
+const timedPromise = function(delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(7.7), delay)
+    })
+}
+
+timedPromise(3000).then(result => console.log(result));
+
+const timedPromiseInArrowFn = delay => new Promise(resolve => {setTimeout(() => resolve(6.67), delay)});
+
+timedPromiseInArrowFn(7000).then(result => console.log(result));

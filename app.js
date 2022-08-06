d196f30fb1eb362b5dd5bd1e7b6c594aa9d19c17
yuzhang <yuzhang2002@gmail.com>
2022-08-06 23:26:38 +1000

    wip

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
-// const simplePlan = `
-// .................
-// ..#...........#..
-// ..#.........=.#..
-// `;
-
-// let rows = simplePlan.trim().split('\n').map(l=>[...l]);
-
-// console.log(rows);
-
-// console.log(Object.prototype.toString.call(rows));
-
-// const trivialArray = [1,2,3];
-// console.log(trivialArray.length);
-// trivialArray['salary'] = 9;
-// console.log(trivialArray['salary']);
-// console.log(trivialArray.length);
-
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(9.88), 1000);
-});
-
-myPromise.then(result => console.log(result));
-
-const timedPromise = function(delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(7.7), delay)
-    })
+class Point {
+    constructor(a, b) {
+        this.x = a;
+        this.y = b;
+    }
 }
 
-timedPromise(3000).then(result => console.log(result));
-
-const timedPromiseInArrowFn = delay => new Promise(resolve => {setTimeout(() => resolve(6.67), delay)});
-
-timedPromiseInArrowFn(7000).then(result => console.log(result));
+const newPoint = new Point(1,2);
+console.log(newPoint.x);

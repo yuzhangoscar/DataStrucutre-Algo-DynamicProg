5960b3fee96f3230e2629bd2966bf1b6476118f4
yuzhang <yuzhang2002@gmail.com>
2022-08-06 23:37:22 +1000

    wip

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 class Point {
+    z = 9.9;
+
     constructor(a, b) {
         this.x = a;
         this.y = b;
+    }
+
+    updateZ(c) {
+        this.z=c;
     }
 }
 
 const newPoint = new Point(1,2);
 console.log(newPoint.x);
+console.log(newPoint.z);
+newPoint.updateZ(7.7);
+console.log(newPoint.z);

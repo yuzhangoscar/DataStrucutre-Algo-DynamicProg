9de48838c17764604abcfa2d7cdb5bd2baf2245b
yuzhang <yuzhang2002@gmail.com>
2022-08-07 16:19:26 +1000

    wip

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-class Point {
-    z = 9.9;
+class User {
+    firstName = "John";
+    lastName = "Doe";
 
-    constructor(a, b) {
-        this.x = a;
-        this.y = b;
+    get random() {
+        console.log(`random getter`);
     }
 
-    updateZ(c) {
-        this.z=c;
-    }
-
-    get z() {
-        return this._z;
-    }
-
-    set z(value) {
-        this._z = value;
+    set random(input) {
+        console.log(`random setter, setting ${input}`);
     }
 }
 
-const newPoint = new Point(1,2);
-console.log(newPoint.x);
-console.log(newPoint.z);
-newPoint.updateZ(7.7);
-console.log(newPoint.z);
+const newUser = new User();
 
-Point.z=10.99;
-console.log(Point.z);
+newUser.random;
+newUser.random = 4.4;

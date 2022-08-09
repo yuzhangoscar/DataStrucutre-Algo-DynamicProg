05a21c3a44e4ed7c201d0c5338b0b5a0cd3bda17
yuzhang <yuzhang2002@gmail.com>
2022-08-09 17:45:44 +1000

    wip

--- app.js
+++ app.js
@@ -8,4 +8,16 @@ Ninja.prototype.swingSword = function() {
     console.log('ninja swings a sword.');
 };
 
+Ninja.prototype = {
+    pierce: function() {
+        console.log('Ninja pierces.');
+    }
+}
+
+const ninjaTwo = new Ninja();
+
 ninjaOne.swingSword();
+//ninjaTwo.swingSword();
+//ninjaOne.pierce();
+ninjaTwo.pierce();
+console.log(ninjaOne.constructor);

08748ae8e8617a965a206c5146b24539f499dc78
yuzhang <yuzhang2002@gmail.com>
2022-08-12 11:03:33 +1000

    wip

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
-const uniqueFunctionStorage = {
-    id: 1,
-    store: function(fn) {
-        console.log(`fn.id is ${fn.id}`);
-        if (!fn.id) {
-            fn.id = this.id++;
-            console.log(`new function stored`);
-        }
-        else
-            console.log(`function already stored`);
-    }
+function fn(a, b) {
+    console.log(a, b);
 }
 
-function One() {};
-function Two() {};
-
-console.log(One.id);
-uniqueFunctionStorage.store(One);
-console.log(One.id);
-uniqueFunctionStorage.store(One);
-console.log(One.id);
-uniqueFunctionStorage.store(Two);
-console.log(Two.id);
-
-if(0) {
-    console.log(`0 is true`);
+const partialFn = function(a) {
+    return fn(a, 'b');
 }
 
-if(!0) {
-    console.log(`0 is false`);
-}
+partialFn('a');

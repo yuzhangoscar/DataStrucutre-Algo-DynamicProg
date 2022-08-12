2f4bae4c74021c925bbca608141c3989508b2949
yuzhang <yuzhang2002@gmail.com>
2022-08-13 09:52:00 +1000

    wip

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-function fn(a, b) {
-    console.log(a, b);
+const storageObj = {
+    id:1,
+    storage:[],
+    hasBeenStored: function(fn){
+        if(!fn.id) {
+            fn.id = this.id++;
+            this.storage.push(fn);
+            console.log(`this function ${fn} was stored.`);
+        }
+        else {
+            console.log(`this function ${fn} has been stored before`);
+        }
+    }
 }
 
-const partialFn = function(a) {
-    return fn(a, 'b');
-}
+function one(){};
+function two(){};
 
-partialFn('a');
+storageObj.hasBeenStored(one);
+storageObj.hasBeenStored(two);
+storageObj.hasBeenStored(one);
+console.log(storageObj.storage);

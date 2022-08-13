b9917ef9570b7c1106196917a16ee1c48e831325
yuzhang <yuzhang2002@gmail.com>
2022-08-13 10:06:19 +1000

    wip

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-function memory(number) {
-    if(!memory.cache) {
-        memory.cache = {};
-    }
-    if (!memory.cache[number]) {
-        memory.cache[number] = true;
-        console.log(`this number was cached`);
-    }
-    else {
-        console.log(`this number has been cached before`);
-    }
-}
+const newPromise = new Promise((resolve, reject) => {
+    setTimeout(()=>console.log(1), 1000);
+});
 
-memory(1);
-memory(2);
-memory(3);
-memory(1);
-console.log(memory);
+newPromise.then(result => console.log(result));

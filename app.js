f0ba62c798aca2b5c7bbba6fcc9d8ed1440ea15f
yuzhang <yuzhang2002@gmail.com>
2022-08-13 10:04:28 +1000

    wip

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-(function(input){console.log(`the input is ${input}`)})(23);
+function memory(number) {
+    if(!memory.cache) {
+        memory.cache = {};
+    }
+    if (!memory.cache[number]) {
+        memory.cache[number] = true;
+        console.log(`this number was cached`);
+    }
+    else {
+        console.log(`this number has been cached before`);
+    }
+}
+
+memory(1);
+memory(2);
+memory(3);
+memory(1);
+console.log(memory);

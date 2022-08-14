cd1f411fb99aba5e6d3b968e56d47b0a101ad21f
yuzhang <yuzhang2002@gmail.com>
2022-08-15 07:55:32 +1000

    wip

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-function fn() {
-    var visibleVar = 9.9;
-    for (var index = 0; index < 4; index++) {
-        var anotherVisibleVar = 8.8;
-        let invisibleVar = 7.7;
-        const anotherInvisibleVar = 6.6;
-    }
-    console.log(anotherVisibleVar);
-    //console.log(invisibleVar);
-    console.log(anotherInvisibleVar);
-}
-
-fn();
+declaredFn();
+//arrowFn();
+//expressedFn();
+function declaredFn() {};
+const arrowFn = () => {};
+const expressedFn = function() {};

f3fbf40911337307b22be998b544c9163dc81e53
yuzhang <yuzhang2002@gmail.com>
2022-08-14 12:33:57 +1000

    wip

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-let outerReferenceToInnerFunction = null;
+// const HiH1 = document.getElementById('hi');
+// const Button = document.getElementById('btn');
 
-function outerFunction() {
-    const varLocalToOuterFunction = 9.9;
-
-    function innerFunction() {
-        console.log(varLocalToOuterFunction);
-    }
-
-    outerReferenceToInnerFunction = innerFunction;
+function animate(id){
+    let count = 0;
+    let element = document.getElementById(id);
+    const action = setInterval(() => {
+        console.log(`current count is: ${count}, element is ${id}`);
+        count++; 
+        if (count >= 100) {
+            clearInterval(action);
+        }
+        element.style.left = element.style.top = count + "px";
+    }, 10);
 }
 
-outerFunction();
-outerReferenceToInnerFunction();
+animate('hi');
+animate('btn');

8cc94744641879e8953fa781893c5e272f1de421
yuzhang <yuzhang2002@gmail.com>
2022-08-15 07:59:02 +1000

    wip

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-declaredFn();
-//arrowFn();
-//expressedFn();
-function declaredFn() {};
-const arrowFn = () => {};
-const expressedFn = function() {};
+var globalVar = 'global var';
+for (let index = 0; index < 10; index++) {
+    let letVar = 'letVar';
+    const constVar = 'constVar';
+}
+console.log(`can see ${globalVar}, but cannot see ${letVar} or ${constVar}`);

50f872a7c51ce8d607da3a295437481a151fcf84
yuzhang <yuzhang2002@gmail.com>
2022-08-16 08:28:01 +1000

    wip

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function* IDGenerator(firstID) {
-    let newID = yield `this is ${firstID}`;
-    let secondNewID = yield `this is an intermediate message that carried the newID value ${newID}`;
-    yield secondNewID;
-    yield `this is an intermedidate message that carried the secondNewID value ${secondNewID}`;
+    let newID = yield firstID;
+    yield `newID is ${newID}`;
+    let secondID = yield newID;
+    yield `secondID is ${secondID}`;
 }
 
-const iterator = IDGenerator('007');
-console.log(iterator.next().value); //this is 007
-console.log(iterator.next('008').value); //this is an intermediate message that carried the newID value 008
-console.log(iterator.next('009').value); //009
-console.log(iterator.next('009').value); //this is an intermediate message that carried the secondNEW ID value 009
+const IDIterator = IDGenerator('007');
+console.log(IDIterator.next('008').value); //007
+console.log(IDIterator.next('009').value); //newID is 008
+console.log(IDIterator.next('009').value); //008
+console.log(IDIterator.next().value); //secondID is 009

0bffcc1fa712a498029ac1c663896a25c194253d
yuzhang <yuzhang2002@gmail.com>
2022-08-17 12:19:50 +1000

    wip

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-function* IDGenerator(firstID) {
-    let newID = yield firstID;
-    yield `newID is ${newID}`;
-    let secondID = yield newID;
-    yield `secondID is ${secondID}`;
+function recursive(number) {
+    if (number > 0) {
+        console.log(number);
+        number--;
+        recursive(number);
+    }
 }
 
-const IDIterator = IDGenerator('007');
-console.log(IDIterator.next('008').value); //007
-console.log(IDIterator.next('009').value); //newID is 008
-console.log(IDIterator.next('009').value); //008
-console.log(IDIterator.next().value); //secondID is 009
+recursive(10);

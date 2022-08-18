41cbc79e0fb422d9c6d3ab3a2b7d4f124230feeb
yuzhang <yuzhang2002@gmail.com>
2022-08-19 08:26:45 +1000

    wip

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-(function IIF(input){
-    console.log(input);
-})('I am invoked.');
+function remember(input) {
+    if(!remember.storage) {
+        remember.storage = {};
+    }
+    if(!remember.storage[input]) {
+        remember.storage[input] = true;
+        console.log(`a new entry was saved.`);
+    }
+    else console.log(`this entry has been saved before`);
+}
+
+remember(1);
+remember(2);
+remember(1);

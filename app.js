9d366ab37f91ac2b4b5ab4dfaf07e87b6143e4aa
yuzhang <yuzhang2002@gmail.com>
2022-08-18 20:20:38 +1000

    wip

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const newPromise = new Promise((resolve, reject) => {
-    resolve(9.9);
+    //resolve(9.9);
+    reject(8.8);
 })
 
-newPromise.then((result) => console.log(result));
+newPromise.then(
+    (result) => console.log(result),
+    (reason) => console.log(reason)
+);

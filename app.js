af44c34429865061b6ed76db9445afdef7e9407c
yuzhang <yuzhang2002@gmail.com>
2022-08-20 11:50:49 +1000

    wip

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-function outerFn() {
-    let outerFnVar = 8.8;
-
-    function innerFn() {
-        console.log(outerFnVar);
-    }
-
-    return innerFn;
+function* generator(input) {
+    const firstYield = yield `first yield: ${input}`;
+    yield `you cannot see this line`;
+    const secondYield = yield `second yield is called, first yield is ${firstYield}`;
 }
 
-const newFn = outerFn();
-
-newFn();
+const iterator = generator(1.1);
+console.log(iterator.next()); //first yield: 1.1
+console.log(iterator.next(1.2)); //you cannot see this line
+console.log(iterator.next()); //second yield is called, first yield is 1.2

0d565de80c11e9bb13594f4e4da4b116165df60c
yuzhang <yuzhang2002@gmail.com>
2022-08-20 14:55:40 +1000

    wip

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-function* generator(input) {
-    const firstYield = yield `first yield: ${input}`;
-    yield `you cannot see this line`;
-    const secondYield = yield `second yield is called, first yield is ${firstYield}`;
-}
-
-const iterator = generator(1.1);
-console.log(iterator.next()); //first yield: 1.1
-console.log(iterator.next(1.2)); //you cannot see this line
-console.log(iterator.next()); //second yield is called, first yield is 1.2
+console.log(-(function(i){return i;})('2'));

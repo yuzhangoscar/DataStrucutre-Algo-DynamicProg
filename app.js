d9ec0e7b706f93a9b723e8322d0011e18cda7033
yuzhang <yuzhang2002@gmail.com>
2022-08-23 15:33:17 +1000

    wip

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const shape = {
-    radius: 10,
-    diameter() {
-      return this.radius * 2;
-    },
-    perimeter: () => 2 * Math.PI * this.radius,
-  };
-  
-  console.log(shape.diameter());
-  console.log(shape.perimeter());
+function scope() {
+    let radius = 9.9;
+    const shape = {
+            radius: 10,
+            diameter() {
+            return this.radius * 2;
+            },
+            perimeter: () => 2 * Math.PI * this.radius,
+    };
+
+    console.log(shape.diameter());
+    console.log(shape.perimeter());
+}
+
+scope();

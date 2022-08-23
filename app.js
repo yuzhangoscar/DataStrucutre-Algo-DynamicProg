2e2fe33418174b8bf49d302f4af9e1125345db0d
yuzhang <yuzhang2002@gmail.com>
2022-08-23 15:20:00 +1000

    wip

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-
-
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1000*i);
-}
+const shape = {
+    radius: 10,
+    diameter() {
+      return this.radius * 2;
+    },
+    perimeter: () => 2 * Math.PI * this.radius,
+  };
+  
+  console.log(shape.diameter());
+  console.log(shape.perimeter());

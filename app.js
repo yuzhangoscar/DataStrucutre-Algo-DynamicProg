4544e989fd1fa0b08784cc472adaac234e63a23b
yuzhang <yuzhang2002@gmail.com>
2022-08-27 19:48:05 +1000

    wip

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-class Person {
-    name = null;
-    age = null;
-    constructor(n, a) {
-        this.name = n;
-        this.age = a;
-    }
-    set Name(value) {
-        this.name = value;
-    }
-    get Name() {
-        return this.name;
-    }
-
-    static whoami() {
-        return this.name + this.age;
-    }
+const obj = {
+    name: null,
+    set Name(value) {this.name = value},
+    get Name() {return this.name}
 }
 
-class Ninja extends Person {
-    constructor(n, a){
-        super(n, a);
-    }
-}
-
-const ninjaOne = new Ninja(`a`, 9);
-ninjaOne.Name = `aa`;
-console.log(ninjaOne.name);
-console.log(Ninja.whoami());
+obj.Name = 'joe';
+console.log(obj.Name);

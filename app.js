19b66e5a742090a65da076b3f47905be2cad8c7a
yuzhang <yuzhang2002@gmail.com>
2022-08-30 17:12:34 +1000

    wip

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const elements = {
-    length: 0,
-    add: function(element) {
-        Array.prototype.push.call(this, element);
-    },
-    find: function(callback) {
-        return Array.prototype.find.call(this, callback);
-    }
-}
+const ninjaCollection = new Map();
 
-console.log(elements);
-elements.add(`ninjaOne`);
-console.log(elements);
-elements.add(`ninjaTwo`);
-console.log(elements);
-console.log(elements.find(element => element==='ninjaOne'));
+const ninjaOne = {name: "hanzo"};
+const ninjaTwo = {name: "Juzo"};
+
+ninjaCollection.set(ninjaOne, {hometown: "Tokyo"});
+ninjaCollection.set(ninjaTwo, {hometown: "Kyoto"});
+
+console.log(ninjaCollection);

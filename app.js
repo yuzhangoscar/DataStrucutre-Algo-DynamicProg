093f35639865b2e21770d9ba4fada37d9ae4fada
sesame <sesame@sesame.tpgi.com.au>
2022-12-01 08:40:05 +1100

    end

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-const getHint = function(secret, guess) {
-    const secretArray = new Array(10).fill(0);
-    const guessArray = new Array(10).fill(0);
-    let secretStringArray = String(secret).split('');
-    let guessStringArray = String(guess).split('');
-    let length = secretStringArray.length;
-    let Bulls = 0;
-    let Cows = 0;
+const set = new Set([1,2,3,4,5,6,-1,-2,-3,-4]);
 
-    for (let index = 0; index < length; index++) {
-        if (secretStringArray[index] === guessStringArray[index]) {
-            Bulls++;
-        }
-        else {
-            secretArray[Number([secretStringArray[index]])]++;
-            guessArray[Number([guessStringArray[index]])]++;
-        }
-    }
-    
-    for (let index = 0 ; index < 10; index++) {
-        Cows += Math.min(secretArray[index], guessArray[index]);
-    }
-
-    return `${Bulls}A${Cows}B`;
-};
-
-console.log(getHint(1919, 1991));
+console.log(set.has(1));
+console.log(set);
+set.forEach((key, value) => {
+    console.log(`key is: ${key}, value is ${value}`);
+});
+set.add({'a':1});
+console.log(set);

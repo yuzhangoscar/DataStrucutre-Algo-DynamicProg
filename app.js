0d5966e39a426e48a48076901b9589c9893ad41c
yuzhang <yuzhang2002@gmail.com>
2022-12-14 09:18:40 +1100

    wip

--- app.js
+++
@@ -1,9 +0,0 @@
-const set = new Set([1,2,3,4,5,6,-1,-2,-3,-4]);
-
-console.log(set.has(1));
-console.log(set);
-set.forEach((key, value) => {
-    console.log(`key is: ${key}, value is ${value}`);
-});
-set.add({'a':1});
-console.log(set);

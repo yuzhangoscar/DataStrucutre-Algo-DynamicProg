7bd0cfc0975d55041cc1560ce55ee130d748e7b1
yuzhangoscar <yuzhang2002@gmail.com>
2023-01-04 10:29:37 +1100

    [HTML5] background foreground images added and animated

--- app.js
+++ app.js
@@ -28,6 +28,23 @@ const game = {
     showLevelScreen: function() {
         game.hideScreens();
         game.showScreen('levelselectscreen');
+    },
+    start: function() {
+        game.hideScreens();
+        game.showScreen('gamecanvas');
+        game.showScreen('scorescreen');
+        game.mode = "intro";
+        game.currentHero = undefined;
+        game.offsetLeft = 0;
+        game.ended = false;
+        game.animationFrame = window.requestAnimationFrame(game.animate, game.canvas);
+    },
+    animate: function() {
+        game.context.drawImage(game.currentLevel.backgroundImage, game.offsetLeft / 4, 0, game.canvas.width, game.canvas.height, 0, 0, game.canvas.width, game.canvas.height);
+        game.context.drawImage(game.currentLevel.foregroundImage, game.offsetLeft / 4, 0, game.canvas.width, game.canvas.height, 0, 0, game.canvas.width, game.canvas.height);
+        if (!game.ended) {
+            game.animationFrame= window.requestAnimationFrame(game.animate, game.canvas);
+        }
     }
 }
 
@@ -57,8 +74,62 @@ const levels = {
             levelSelectScreen.appendChild(button);
         }
     },
-    load: function(number) {}
+    load: function(number) {
+        game.currentLevel={number:number};
+        game.score=0;
+
+        document.querySelector('#score').innerHTML = 'score: '+ game.score;
+        let level = levels.data[number];
+
+        game.currentLevel.backgroundImage = loader.loadImage('resources/icons/backgrounds/' + level.background + '.png');
+        game.currentLevel.foregroundImage = loader.loadImage('resources/icons/backgrounds/' + level.foreground + '.png');
+        
+        loader.onload = game.start;
+    }
 };
+
+const loader = {
+    loaded: false,
+    loadedCount: 0,
+    totalCount: 0,
+    init: function() {
+    },
+    loadImage: function(url) {
+        this.loaded = false;
+        this.totalCount++;
+        game.showScreen('loadingscreen');
+        let image = new Image();
+        image.addEventListener('load', loader.itemLoaded, false);
+        image.src = url;
+
+        return image;
+    },
+    loadSound: function(url) {
+        this.loaded = false;
+        this.totalCount++;
+        game.showScreen('loadingscreen');
+        let audio = new Audio();
+        audio.addEventListener('canplaythrough', loader.itemLoaded, false);
+        audio.src = url + '.mp3';
+
+        return audio;
+    },
+    itemLoaded: function(ev) {
+        loader.loadedCount++;
+        document.querySelector('#loadingmessage').innerHTML = 'Loaded' + loader.loadedCount + ' of' + loader.totalCount;
+        if(loader.loadedCount === loader.totalCount) {
+            loader.loaded = true;
+            loader.loadedCount = 0;
+            loader.totalCount = 0;
+
+            game.hideScreen('loadingscreen');
+            if(loader.onload) {
+                loader.onload();
+                loader.onload = undefined;
+            }
+        }
+    }
+}
 
 window.addEventListener('load', () => {
     console.log('page has been loaded.');

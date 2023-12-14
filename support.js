ac7cf963512142e9fcffb7742574db427a39f735
yuzhang <yuzhang2002@gmail.com>
2023-12-15 08:25:58 +1100

    removed all

--- support.js
+++
@@ -1,4 +0,0 @@
-const timedPromise = delay => new Promise((resolve, reject) => setTimeout(() => resolve('resolved'), delay));
-setTimeout(() => console.log('5 seconds'), 5000);
-
-export {timedPromise}

b798eb729ac1dee8a6bf26feb724e83c1d283e90
yuzhang <yuzhang2002@gmail.com>
2024-01-24 20:57:16 +1100

    [PROJECT] authentication works now, Google Auth

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -3,10 +3,23 @@ require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./server/config/db');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const port = 3000 || process.env.PORT;  
 
+app.use(session({
+    secret: 'mySecret',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 connectDB();
@@ -18,6 +31,7 @@ app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
+app.use('/', require('./server/routes/oauth'));
 app.use('/', require('./server/routes/index'));
 
 //404
---
+++ nodeJS/server/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    googleId: {
+        type: String,
+        required: true
+    }, 
+    displayName: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    profileImage: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
